a3ccb32257628234d29778a43f643641ab91cce0
byeongchae <byeongchae44@gmail.com>
2024-03-22 15:44:58 +0900

    깃허브 이슈 가져오기

--- issue-reminder.js
+++ issue-reminder.js
@@ -1,12 +1,26 @@
-import { Octokit } from "octokit";
+import {Octokit} from "octokit";
 
 const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN
-})
+     auth: process.env.GITHUB_TOKEN
+});
 
-const issues = octokit.rest.issues.listForRepo({
+const response = await octokit.request("GET /repos/{owner}/{repo}/issues", {
     owner: "kkatalchae",
     repo: "github_issue_reminder",
-    per_page: 10,
-})
+    state: "open",
+
+});
+
+const issues = response?.data ?? [];
+
+for (const issue of issues) {
+
+    console.log(
+        `
+        ${issue?.title ?? '이슈 제목'} ${issue?.created_at ?? '생성일자'}
+
+        ${issue?.assignees?.length === 0 ? '담당자 지정이 필요합니다' : ''}
+        `
+    )
+}
 
